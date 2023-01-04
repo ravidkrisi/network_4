3476894a7895e8aa78615481f378a83ea8299c2e
ravidkrisi <ravidkrisi22@gmail.com>
2023-01-04 14:29:58 +0200

    final part 1 of the project

--- ping.c
+++ ping.c
@@ -1,3 +1,9 @@
+// icmp.cpp
+// Robert Iakobashvili for Ariel uni, license BSD/MIT/Apache
+//
+// Sending ICMP Echo Requests using Raw-sockets.
+//
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
@@ -16,6 +22,133 @@
 
 // ICMP header len for echo req
 #define ICMP_HDRLEN 8
+
+// Checksum algo
+unsigned short calculate_checksum(unsigned short *paddress, int len);
+
+
+int main(int argc, char *argv[])
+{
+    //set vars
+    struct icmp icmphdr; // ICMP-header
+    struct sockaddr_in dest_in;
+    struct timeval start, end;
+
+    char data[IP_MAXPACKET] = "This is the ping.\n";
+    char reply_ip[IP_MAXPACKET]={0};
+
+    int sequence=-1;
+    int i=0;
+    int ttl=-1;
+    int datalen = strlen(data) + 1;
+
+
+    //check that we received 2 arguments 
+    if(argc!=2)
+    {
+        printf("error with passed arguemnts\n");
+        exit(1);
+    }
+    //check the validation of the IP address we received and insert it to dest_in
+    memset(&dest_in, 0, sizeof(struct sockaddr_in));
+    if((inet_pton(AF_INET, argv[1], &dest_in.sin_addr))<=0)
+    {
+        printf("received invalid IP address");
+    }
+
+    // Create raw socket for IP-RAW (make IP-header by yourself)
+    int sock = -1;
+    if ((sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
+    {
+        fprintf(stderr, "socket() failed with error: %d", errno);
+        fprintf(stderr, "To create a raw socket, the process needs to be run by Admin/root user.\n\n");
+        return -1;
+    }
+
+
+    //while inifinte loop
+    while(1){
+    //===================
+    // ICMP header
+    //===================
+
+    // Message Type (8 bits): ICMP_ECHO_REQUEST
+    icmphdr.icmp_type = ICMP_ECHO;
+
+    // Message Code (8 bits): echo request
+    icmphdr.icmp_code = 0;
+
+    // Identifier (16 bits): some number to trace the response.
+    // It will be copied to the response packet and used to map response to the request sent earlier.
+    // Thus, it serves as a Transaction-ID when we need to make "ping"
+    icmphdr.icmp_id = 18;
+
+    // Sequence Number (16 bits): starts at 0
+    icmphdr.icmp_seq = i;
+    i++;
+    // ICMP header checksum (16 bits): set to 0 not to include into checksum calculation
+    icmphdr.icmp_cksum = 0;
+
+    // Combine the packet
+    char packet[IP_MAXPACKET];
+
+    // Next, ICMP header
+    memcpy((packet), &icmphdr, ICMP_HDRLEN);
+
+    // After ICMP header, add the ICMP data.
+    memcpy(packet + ICMP_HDRLEN, data, datalen);
+
+    // Calculate the ICMP header checksum
+    icmphdr.icmp_cksum = calculate_checksum((unsigned short *)(packet), ICMP_HDRLEN + datalen);
+    memcpy((packet), &icmphdr, ICMP_HDRLEN);
+
+ 
+
+    //set start time
+    gettimeofday(&start, 0);
+
+    // Send the packet using sendto() for sending datagrams.
+    int bytes_sent = sendto(sock, packet, ICMP_HDRLEN + datalen, 0, (struct sockaddr *)&dest_in, sizeof(dest_in));
+    if (bytes_sent == -1)
+    {
+        fprintf(stderr, "sendto() failed with error: %d", errno);
+        return -1;
+    }
+
+    // Get the ping response
+    bzero(packet, IP_MAXPACKET);
+    socklen_t len = sizeof(dest_in);
+    int bytes_received = -1;
+    while ((bytes_received = recvfrom(sock, packet, sizeof(packet), 0, (struct sockaddr *)&dest_in, &len)))
+    {
+        if (bytes_received > 0)
+        {
+            // set the end time and caculating ping time
+            gettimeofday(&end, 0);
+            double t_send_reply = ((end.tv_sec - start.tv_sec)*1000) + (((double)end.tv_usec-start.tv_usec)/1000);
+            //get ip source of the packet
+            struct iphdr *iphdr_rep = (struct iphdr *)packet;
+            struct icmphdr *icmphdr_rep = (struct icmphdr *)(packet + (iphdr_rep->ihl * 4));
+            inet_ntop(AF_INET, &(iphdr_rep->saddr), reply_ip, INET_ADDRSTRLEN);
+            //get ttl and sequence of the packet
+            ttl= iphdr_rep->ttl;
+            sequence = icmphdr_rep->un.echo.sequence;
+
+            //print all the informtion the packet: bytes, source ip, ttl, icmp_seq, time to ping
+            printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%f ms\n", bytes_received, reply_ip, sequence, ttl, t_send_reply);
+
+            break;
+        }
+    }
+    //put the prog to sleep for 1 second to better see the output 
+    sleep(1);
+    }
+
+    // Close the raw socket descriptor.
+    close(sock);
+
+    return 0;
+}
 
 // Compute checksum (RFC 1071).
 unsigned short calculate_checksum(unsigned short *paddress, int len)
@@ -43,111 +176,4 @@ unsigned short calculate_checksum(unsigned short *paddress, int len)
     answer = ~sum;                      // truncate to 16 bits
 
     return answer;
-}
-
-int main(int argc, char *argv[])
-{
-    int sockfd=-1;
-    int datalen=-1;
-    int sequence=-1;
-    int i=0;
-    int ttl=-1;
-    struct icmp icmphdr;
-    struct sockaddr_in dest_in;
-    struct timeval start, end;
-    struct iphdr *iphdr_rep;
-    struct icmphdr *icmphdr_rep;
-    char packet[IP_MAXPACKET]={0};
-    char packet_recv[IP_MAXPACKET]={0};
-    char reply_ip[INET_ADDRSTRLEN]={0};
-
-
-    //check that we received 2 arguments 
-    if(argc!=2)
-    {
-        printf("error with passed arguemnts\n");
-        exit(1);
-    }
-    //check the validation of the IP address we received and insert it to dest_in
-    memset(&dest_in, 0, sizeof(struct sockaddr_in));
-    if((inet_pton(AF_INET, argv[1], &dest_in.sin_addr))<=0)
-    {
-        printf("received invalid IP address");
-    }
-    dest_in.sin_family = AF_INET;
-
-
-    //set messeage to data and the length of the message
-    char data[IP_MAXPACKET]= "The is the ping.\n";
-    int data_len = strlen(data)+1;
-
-    //set icmphdr fields to ICMP_ECHO type
-    icmphdr.icmp_type = ICMP_ECHO;
-    icmphdr.icmp_code = 0; 
-    icmphdr.icmp_id = 18;
-    icmphdr.icmp_cksum = 0;
-    icmphdr.icmp_seq = i;
-    i++;
-
-    //copy to icmp header to the packet we will send 
-    memcpy((packet), &icmphdr, ICMP_HDRLEN);
-
-    //copy the data to the location after the icmp header
-    memcpy(packet+ICMP_HDRLEN, data, datalen);
-
-    //calculate the ICMP header checksum and copy the ICMP header again
-    icmphdr.icmp_cksum = calculate_checksum((unsigned short *)(packet), ICMP_HDRLEN +data_len);
-    memcpy((packet), &icmphdr, ICMP_HDRLEN);
-
-    //create raw socket 
-    if((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
-    {
-        printf("failed to create a raw socket");
-        return -1;
-    }
-    //set while infinte loop 
-    while(1){
-
-    //set the start time of sending one packet
-    gettimeofday(&start, 0);
-
-    //sending the packet and checking if it was sent
-    int bytes_sent = sendto(sockfd, packet, ICMP_HDRLEN+data_len, 0, (struct sockaddr *)&dest_in, sizeof(dest_in));
-    if(bytes_sent == -1)
-    {
-        printf("sendoto() failed\n");
-        return -1;
-    }
-
-    //receive the reply 
-    bzero(packet_recv, IP_MAXPACKET);
-    socklen_t len = sizeof(dest_in);
-    int bytes_received = -1;
-
-    while(bytes_received = recvfrom(sockfd, packet_recv, sizeof(packet_recv), 0, (struct sockaddr *)&dest_in, &len))
-    {
-        if(bytes_received>0)
-        {
-            //set the end time and caculate the time it took to send and receive reply 
-            gettimeofday(&end, 0);
-            double t_send_reply = ((end.tv_sec - start.tv_sec)*1000) + (((double)end.tv_usec-start.tv_usec)/1000);
-
-            //check the ip header and insert the ip reply to string 
-            iphdr_rep = (struct iphdr *)packet_recv; 
-            icmphdr_rep = (struct icmphdr *)(packet_recv + (iphdr_rep->ihl * 4));
-            inet_ntop(AF_INET, &(iphdr_rep->saddr), reply_ip, INET_ADDRSTRLEN);
-            sequence = ntohs(icmphdr_rep->un.echo.sequence);
-            ttl = iphdr_rep->ttl;
-
-            //print bytes received in a packet, ip of the reply packet, sequence number, ttl, and time it took to send and reply
-            printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%f ms\n", bytes_received, reply_ip, sequence, ttl, t_send_reply);
-            break;
-        }
-    }
-
-    sleep(1);
-    }
-    //close to RAW socket
-    close(sockfd);
-    return 0;
 }
